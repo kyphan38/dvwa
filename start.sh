a92f5fa530ff7da6a9897325ee8f606654a2506a
kyphan38 <kyphan.work@gmail.com>
2024-07-11 07:43:45 +0700

    pattern

--- start.sh
+++ start.sh
@@ -10,4 +10,10 @@ echo "Container id of dvwa: $dvwa_container_id" # Var only used while executing 
 
 sed -i -E "s|containers/[^/]+/[^-]+-json\.log|containers/$dvwa_container_id/$dvwa_container_id-json.log|g" compose.yml
 
+if grep -q "containers/$dvwa_container_id/$dvwa_container_id-json.log" compose.yml; then
+    echo "Log path updated successfully"
+else
+    echo "Failed to update log path"
+fi
+
 docker compose up --build -d elasticsearch kibana logstash
